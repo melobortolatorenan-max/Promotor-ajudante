bd44b050ee82c4c43c664f3a31a6273e19f82e60
melobortolatorenan-max <melobortolato.renan@gmail.com>
2026-04-04 10:07:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,8 @@
-const CACHE_NAME = 'promotor-v10';
-
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
+const CACHE_NAME = 'promotor-v11';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
 });
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys.map(key => caches.delete(key)));
-        })
-    );
-});
-
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
